643eb8865f75517b711f46616aebd67a02d652dc
Lucas Sena <lucassena.rj@gmail.com>
2024-09-09 15:07:53 +0100

    initial commit

---
+++ nvim/lua/lucassenarj/init.lua
@@ -0,0 +1,3 @@
+require("lucassenarj.remap")
+require("lucassenarj.theme")
+
---
+++ nvim/lua/lucassenarj/theme.lua
@@ -0,0 +1,13 @@
+vim.cmd.colorscheme('slate')
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+vim.opt.guicursor = ""
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 24
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
